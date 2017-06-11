50c6f4d0655d73f7e8da4223b0ad40f33722b13d
Joe (Hasee) <lsz2006.ad@gmail.com>
2017-06-11 19:41:14 +0100

    edit Gaussian function

--- cuda/util.h
+++ cuda/util.h
@@ -6,6 +6,7 @@
 #ifndef M_PI
 #define M_PI       3.14159265358979323846
 #endif
+
 /**
 http://stackoverflow.com/questions/11209115/creating-gaussian-filter-of-required-length-in-python
 from math import pi, sqrt, exp
@@ -13,26 +14,27 @@ def gauss(n=11,sigma=1):
 r = range(-int(n/2),int(n/2)+1)
 return [1 / (sigma * sqrt(2*pi)) * exp(-float(x)**2/(2*sigma**2)) for x in r]
 */
-std::vector<float> gaussian_kernel_1d(int n = 11, float sigma = 1)
+
+void gaussian_kernel_1d(float kernel[], const int n = 11, const float sigma = 1)
 {
-	std::vector<float> kernel;
 	std::cout << "gaussian_kernel_1d" << std::endl;
+	int j = 0;
 	for (int x = -(n / 2); x < (n / 2) + 1; x++)
 	{
 		float val = 1 / (sigma * sqrt(2 * M_PI)) * exp(-x*x / (2. * sigma*sigma));
-		kernel.push_back(val);
+		kernel[j++] = val;
 		std::cout << val << " ";
 	}
 	std::cout << std::endl;
-	return kernel;
 }
 
-void gaussian(float4 tf[], int count, int kernel_size = 5, float sigma = 1)
+void gaussian(float4 tf[], const int count, const int kernel_size = 5, const float sigma = 1)
 {
+	int half = kernel_size / 2;
+	auto kernel = (float*)malloc(kernel_size * sizeof(float));
+	gaussian_kernel_1d(kernel, kernel_size, sigma);
 	auto data = (float4*)malloc(count * sizeof(float4));
 	memcpy(data, tf, count * sizeof(float4));
-	int half = kernel_size / 2;
-	auto kernel = gaussian_kernel_1d(kernel_size, sigma);
 	for (int i = half; i < count - half; i++)
 	{
 		float4 sum = make_float4(0, 0, 0, 0);
@@ -45,6 +47,7 @@ void gaussian(float4 tf[], int count, int kernel_size = 5, float sigma = 1)
 	}
 	memcpy(tf, data, count * sizeof(float4));
 	free(data);
+	free(kernel);
 }
 
 /// Re-maps a number from one range to another.
