fc34b1a2513d5efba504ff5f59e5e4ce0ad5588b
Joe (Hasee) <lsz2006.ad@gmail.com>
2017-06-12 01:47:22 +0100

    add transfer functions

--- cuda/my_volumeRender.cpp
+++ cuda/my_volumeRender.cpp
@@ -107,7 +107,7 @@ const char *sSDKsample = "CUDA 3D Volume Render";
 //cudaExtent volumeSize = make_cudaExtent(416, 512, 112);
 //typedef unsigned short VolumeType;
 
-const char *tfs[] = { "nucleon_naive_proportional_2.tfi","vortex_naive_proportional_2.tfi","CT-Knee_naive_proportional_2.tfi" };
+const char *tfs[] = { "nucleon_naive_proportional_2.tfi","vortex_naive_proportional_2.tfi","CT-Knee_CT-Bone5_even_2.tfi" };
 const char *volumes[] = { "nucleon.raw","vorts1.raw","CT-Knee.raw" };
 const int data_index = 2;
 const char *tfFile = tfs[data_index];
