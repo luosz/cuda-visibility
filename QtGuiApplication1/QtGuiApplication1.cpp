f27da939d0eee78812f05cc2726a2d4cdbcbfe53
Joe <lsz2006.ad@gmail.com>
2017-06-29 16:21:39 +0100

    add QtGuiApplication1

---
+++ QtGuiApplication1/QtGuiApplication1.cpp
@@ -0,0 +1,8 @@
+#include "QtGuiApplication1.h"
+
+QtGuiApplication1::QtGuiApplication1(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	ui.gridLayout->addWidget(&glWidget);
+}
---
+++ QtGuiApplication1/QtGuiApplication1.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_QtGuiApplication1.h"
+#include "glwidget.h"
+
+class QtGuiApplication1 : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	QtGuiApplication1(QWidget *parent = Q_NULLPTR);
+
+private:
+	Ui::QtGuiApplication1Class ui;
+	GLWidget glWidget;
+};
