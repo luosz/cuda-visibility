6e9619730d66caa19ce84cb8c02c837718c5b259
Joe (Hasee) <lsz2006.ad@gmail.com>
2018-02-19 21:12:18 +0000

    edit mainwindow.cpp

--- QtCuda/mainwindow.cpp
+++ QtCuda/mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
 			qApp->desktop()->availableGeometry()
 		)
 	);
-	this->move(this->pos() - QPoint(400, 260));
+	this->move(this->pos() - QPoint(400, 300));
 	ui.setupUi(this);
 
 	update_color(QColor::fromRgbF(D_RGBA[0], D_RGBA[1], D_RGBA[2], D_RGBA[3]));
